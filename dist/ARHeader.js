de3f9fa6eba95dad601c300b05cc1e57b7a17e68
jos <7629076+YosefAshenafi@users.noreply.github.com>
2023-12-13 18:00:53 +0300

    updated the package

---
+++ dist/ARHeader.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var ARHeader = function ARHeader() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "This is the ARHeader component");
+};
+var _default = exports["default"] = ARHeader;
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ARHeader", {
+  enumerable: true,
+  get: function get() {
+    return _ARHeader["default"];
+  }
+});
+var _ARHeader = _interopRequireDefault(require("./src/ARHeader"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
